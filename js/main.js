f64030b3fa630acb2bd0850eed8b6fa7ce616544
kubara <masahiko_kubara@relationsgroup.co.jp>
2014-08-21 17:21:30 +0900

    add: comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
+
+//ゲームに必要なライブラリの初期化
 enchant();
 
+//ゲームで使用する定数値
+//定数値はすべて大文字で書きましょう（規約として）
+var GAME_WIDTH = 320;
 var GAME_HEIGHT = 480;
 
 //メインプログラム
 window.onload = function() {
     //ゲームオブジェクトの生成
-    core = new enchant.levelapp.Core(320, GAME_HEIGHT);
+    core = new enchant.levelapp.Core(GAME_WIDTH, GAME_HEIGHT);
+
+    //ゲームの背景色
     core.rootScene.backgroundColor = "rgb(80,239,255)";
+
+    //ゲームID
+    //開発するゲームごとに発行されます。必ず変更して下さい。
+    //ハイスコアサーバに影響します
     core.gameId = "111111";
 
     //画像の読み込み
+    //使用する画像や音声は、ここで読み込んでおいて下さい
     core.preload( './img/start.png'
       , './img/end.png'
       , './img/cogoo.png'
       , './img/map9.png'
-      , './snd/cogoo_voice0.mp3'
     );
 
     //ゲーム開始
